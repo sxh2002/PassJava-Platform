7e3c0627c3dbef6dc675c8aaecf7eab9e4d9d68a
sxh <1342503240@qq.com>
2022-09-03 09:50:49 +0800

    初始化项目

---
+++ passjava-question/src/main/java/com/wwh/passjavaquestiom/PassjavaQuestiomApplication.java
@@ -0,0 +1,13 @@
+package com.wwh.passjavaquestiom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassjavaQuestiomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassjavaQuestiomApplication.class, args);
+    }
+
+}
